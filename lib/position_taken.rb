15c0f67c6575302ce03341d07559a667cf352a01
Max Rabin <max.rabin17@gmail.com>
2021-02-17 15:31:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board == "X or O"
-    return true
+    puts true
   else
-    false
+    puts false
 
 board[index] == " "
 end
